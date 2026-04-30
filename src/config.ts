a5bf4038e4234cf9080370682f5f520b0359bbf9
PEYOITE <peyoite233@outlook.com>
2026-04-30 15:31:45 +0800

    项目初始化

--- src/config.ts
+++ src/config.ts
@@ -45,8 +45,8 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Archive,
 		LinkPreset.About,
 		{
-			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
+			name: "追番",
+			url: "https://bgm.tv/anime/list/peyoite233", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
